d411b21190996c1bafe238ff998c747e37db8c29
imogenkraak <imogen.kraak@gmail.com>
2020-10-17 22:38:36 +0100

    user name input is displayed on scorescene

--- scoreScene.js
+++ scoreScene.js
@@ -6,14 +6,7 @@ class scoreScene extends Phaser.Scene {
      
   
     create() {
-
-        // var printText = this.add.text(400, 200, '', {
-        //     fontSize: '12px',
-        // }).setOrigin(0.5).setFixedSize(100, 100);
-
-
-  
-        
+ 
         this.add.image(640, 360, 'sea')
   
         this.add.text(550, 60, 'HIGH SCORES', { fontFamily: 'Helvetica Neue' })
@@ -22,23 +15,13 @@ class scoreScene extends Phaser.Scene {
         this.add.text(550, 110, gameOptions.scores)
 
 
-       
-
-
-
-
-
-
-
-  
-
+    
   
         var again = this.add.image(640, 360, 'playAgain')
   
         again.setInteractive();
   
         again.on('pointerdown', () => { 
-        this.y += 100
         this.scene.start("PlayGame");
     
     });
