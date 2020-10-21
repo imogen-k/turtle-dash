aba1f470652ee3b7db3795d502802ef63c69c4a8
imogenkraak <imogen.kraak@gmail.com>
2020-10-21 15:16:48 +0100

    change to how to play screen

--- howtoplayScene.js
+++ howtoplayScene.js
@@ -8,7 +8,7 @@ class howToPlay extends Phaser.Scene {
   create() {
     this.add.image(640, 360, 'sea');
     this.add.text(540, 20, 'How to play', { fontFamily: 'bubble_bobbleregular', fontSize: '50px' });
-    this.add.text(410, 80, 'Controls: Use Spacebar/tap to swim up & down', { fontFamily: 'bubble_bobbleregular', fontSize: '25px' });
+    this.add.text(410, 80, 'Controls: Click/tap to swim up & down', { fontFamily: 'bubble_bobbleregular', fontSize: '25px' });
     this.add.text(250, 110, 'Aim of the game: Swim for as long as possible before the timer reaches zero', { fontFamily: 'bubble_bobbleregular', fontSize: '25px' });
     this.add.image(640, 380, 'howtoplay');
 
